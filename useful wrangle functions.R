440295d2cf9bec16e3d43a0972c166e9f81a848c
anslymcknze <amackenzie22@amherst.edu>
2021-03-24 21:11:03 -0400

    just cleaning up from last time

---
+++ useful wrangle functions.R
@@ -0,0 +1,38 @@
+
+
+### useful functions should have memorized by now/know off top of head
+### (may not necessarily know all the input arguments of each function, 
+###  but function name should pop to mind when would be helpful to use in
+###  a given situation)
+
+# select()
+# filter()
+# mutate()
+# rename()
+# spread()
+# gather()
+# group_by()
+# summarise()
+# paste()
+# full_join()
+# inner_join()
+# left_join()
+# right_join()
+# anti_join()
+# separate()
+# unite()
+
+### examples of other useful functions that could be helpful
+### while wrangling the Clearinghouse Challenge datasets
+
+# case_when()
+# str_detect()  (or grepl())
+# str_to_upper()
+# str_to_lower()
+
+
+Helpful websites:
+  https://www.r-graph-gallery.com/
+  https://docs.google.com/spreadsheets/d/1wZhPLMCHKJvwOkP4juclhjFgqIY8fQFMemwKL2c64vk/edit#gid=0
+https://shiny.rstudio.com/gallery/widget-gallery.html
+
